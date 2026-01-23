5fe9cd047cc2a0cb6bee0adb62dda11bc58db6a4
saad <mdsaad.ec31@gmail.com>
2026-01-23 21:41:44 +0530

    Add files via upload

---
+++ Exercise1_Basics/basic.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class basic {
+	
+	public static void main(String[] args) {
+		int a =10;
+		double b = 3.0;
+		char letter = 'A';
+		boolean isJavaFun = true;
+		
+		int sum = a+ (int) b;
+		double division =a/b;
+		double remainder = a%b;
+		double multiplication = a*b;
+		
+		String msg ="VALUE OF a:" +a+",b:"+b;
+		System.out.println("2462618");
+		System.out.println(msg);
+		System.out.println("sum= " +sum);
+		System.out.println("division= " +division);
+		System.out.println("remainder= " +remainder);
+		System.out.println("letter= " +letter+ ", isJavaFun"+isJavaFun);
+		System.out.println("multiplication= " +multiplication);
+	}
+}
